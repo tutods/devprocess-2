83744d1f13ca667f61ff96b536887f03c66f16bc
Daniel Sousa <jdaniel.asousa@gmail.com>
2023-12-12 21:41:50 +0000

    feat: responsive

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,4 @@
 const slider = new Swiper('.videos .slider', {
-  slidesPerView: 3,
-  spaceBetween: 28,
   speed: 800,
   pagination: {
     el: '.videos .swiper-pagination',
@@ -9,5 +7,21 @@ const slider = new Swiper('.videos .slider', {
   navigation: {
     nextEl: '.videos .btn-next',
     prevEl: '.videos .btn-prev'
-  }
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 1.1,
+      spaceBetween: 16,
+    },
+    767: {
+      slidesPerView: 2.1,
+    },
+    1024: {
+      slidesPerView: 3.1,
+    },
+    1200: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+  },
 });
